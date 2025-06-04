06476bb44b08cef8cdd85e65198ba7b1b90a305c
Juan C Martinez <git@jcmart.dev>
2025-06-04 13:23:40 -0500

    Learned about partial prerendering

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    
+// Testing out PPR    
+/*  experimental: {
+      ppr:'incremental'
+  } */ 
 };
 
 export default nextConfig;
